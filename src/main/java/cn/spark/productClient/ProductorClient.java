5ef868ac207735a5e30d114c3e988070d3c69d8f
wtj199101 <422434668@qq.com>
2017-06-23 17:55:53 +0800

    增加ignore file

--- src/main/java/cn/spark/productClient/ProductorClient.java
+++ src/main/java/cn/spark/productClient/ProductorClient.java
@@ -1,9 +1,14 @@
 package cn.spark.productClient;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -21,8 +26,54 @@ public class ProductorClient {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        for(int i = 0; i < 10; i++)
-            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)+"hello,world"));
+            producer.send(new ProducerRecord<String, String>("test", "1",SendMessage()));
         producer.close();
+    }
+
+    public static String SendMessage(){
+        String serverName ="root"; //request.getServerName();
+        Integer serverPort = 8080;//request.getServerPort();
+        String serverPath = "/";//request.getContextPath();
+
+        String localName = "root"; //request.getLocalName();
+        String localAddr ="root"; // request.getLocalAddr();
+        Integer localPort = 1;//request.getLocalPort();
+
+        String remoteHost = "root"; //request.getRemoteHost(); // 客户端主机名
+        String remoteAddr = "root"; //request.getRemoteAddr(); // 客户端ip地址
+        Integer remotePort =1;// request.getRemotePort(); // 客户端端口号
+
+        String protocol = "root"; //request.getProtocol();
+        String schema = "root"; //request.getScheme();
+
+        String method ="root"; // request.getMethod();
+//        Map<String, String[]> parameterMap = request.getParameterMap();
+        String queryString ="root"; // request.getQueryString();
+
+        String requestURI = "root"; //request.getRequestURI();
+        String requestURL ="root"; // request.getRequestURL().toString();
+
+        // 构造 map
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("requestHeaderMap","root"); // requestHeaderMap);
+        map.put("responseHeaderMap", "root"); //responseHeaderMap);
+        map.put("serverName", serverName);
+        map.put("serverPort", serverPort);
+        map.put("serverPath", serverPath);
+        map.put("localName", localName);
+        map.put("localAddr", localAddr);
+        map.put("localPort", localPort);
+        map.put("remoteHost", remoteHost);
+        map.put("remoteAddr", remoteAddr);
+        map.put("remotePort", remotePort);
+        map.put("protocol", protocol);
+        map.put("schema", schema);
+        map.put("method", method);
+        map.put("parameterMap", "12");//parameterMap);
+        map.put("queryString", queryString);
+        map.put("requestURI", requestURI);
+        map.put("requestURL", requestURL);
+        map.put("visitDate", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+        return JSONObject.toJSONStringWithDateFormat(map,"yyyy-MM-dd HH:mm:ss.SSS");
     }
 }
